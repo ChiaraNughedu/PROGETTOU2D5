3f0565727e658666080a10de56f8ff91c2a0e6ca
ChiaraNughedu <chianughedu97@gmail.com>
2024-11-29 17:36:44 +0100

    ultimojs

--- U2-D5/base_di_partenza/assets/js/script.js
+++ U2-D5/base_di_partenza/assets/js/script.js
@@ -1,14 +1,14 @@
 const header = document.querySelector("header");
 const button = document.getElementById("button");
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 180) {
+    if (window.scrollY > 190) {
         header.style.backgroundColor = 'white';
     } else {
         header.style.backgroundColor = '#ffc017';
     }
 });
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 180) {
+    if (window.scrollY > 190) {
         button.style.backgroundColor = 'green';
     } else {
         button.style.backgroundColor = 'black';
